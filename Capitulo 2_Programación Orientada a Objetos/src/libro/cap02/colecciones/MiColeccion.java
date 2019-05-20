314d18a66425013090956cede9b2cccf1f05e8c4
Molina Torres Cecila Paola <cpmolinatorres@gmail.com>
2019-05-19 23:06:24 -0300

    Colecciones

--- Capitulo 2_Programación Orientada a Objetos/src/libro/cap02/colecciones/MiColeccion.java
+++ Capitulo 2_Programación Orientada a Objetos/src/libro/cap02/colecciones/MiColeccion.java
@@ -9,5 +9,62 @@ public class MiColeccion {
 		datos=new Object[capacidadInicial];
 	}
 	
+	//retorna el i-esimo elemento de la coleccion
+	public Object obtener(int i) {
+		return datos[i];
+	}
 	
+	//indica cuantos elementos tiene la coleccion
+	public int cantidad() {
+		return len;
+	}
+	
+	public void insertar(Object elm, int i) {
+		//verificamos si la capacidad del array esta colmada
+		if(len==datos.length) {
+			//creamos un nuevo array con el doble de la capacidad del anterior
+			Object aux[]=datos;
+			datos=new Object[datos.length*2];
+			//copiamos en este los elementos de la coleccion 
+			for(int j=0; j<len; j++) {
+				datos[j]=aux[j];
+			}
+			//asignamos null al anterior para que el Garbage Collector libere la memoria
+			aux=null;
+		}
+		
+		//desplazamos los elementos del array entre la ultima y la i-enesima posicion
+		//para poder asignar en esta el elemento que se presente insertar
+		for(int j=len-1; j>=i; j--) {
+			datos[j+1]=datos[j];
+		}
+		datos[i]=elm;
+		//incrementamos el valor de la variable len
+		len++;
+	}
+	
+	//este metodo recorre el array mientras no se encuente el elemento que busca
+	public int buscar (Object elm) {
+		int i=0;
+		//mientras no nos pasemos del tope y mientra no encuentre...
+		for(;i<len && !datos[i].equals(elm);i++);
+			//si no nos pasamos entonces encontre, sino ...no encontre
+		return i<len ? i:-1;	
+	}
+	
+	//su objetivo es agregar un elemento al final deel array se resuelve invocando al metodo "insertar"
+	//para insetar el elemento en la posicion len
+	public void agregar(Object elm) {
+		insertar(elm,len);
+	}
+	
+	//elimina un elemento desplazando los demas hacia arriba
+	public Object eliminar(int i) {
+		Object aux=datos[i];
+		for(int j=i; j<len-1;j++) {
+			datos[j]=datos[j+1];
+		}
+		len--;
+		return aux;
+	}
 }
---
+++ Capitulo 2_Programación Orientada a Objetos/src/libro/cap02/colecciones/TestMiColeccion.java
@@ -0,0 +1,30 @@
+package libro.cap02.colecciones;
+import java.util.Scanner;
+
+public class TestMiColeccion {
+//muestra una lista de nombres en orden inverso
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		//creo una coleccion con capacidad inicial=5
+		MiColeccion mc=new MiColeccion(5);
+		//leo el primer nombre
+		System.out.println("Ingrese Nombre: ");
+		String nom=scanner.next();
+		while(!nom.equals("FIN")) {
+			//inserto siempre en la posicion cero
+			mc.insertar(nom, 0);
+			//leo el siguiente nombre
+			nom=scanner.next();
+		}
+		String aux;
+		//recorro la coleccion y tomo cada uno de sus elementos
+		for(int i=0; i<mc.cantidad(); i++) {
+			//el metodo obtener retorna un Object entonces tenemos
+			//que castear a String
+			aux=(String) mc.obtener(i);
+			System.out.println(aux+"-"+aux.length()+" caracteres");
+		}
+	}
+
+}
